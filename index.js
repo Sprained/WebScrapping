9c66514b4071abda6abc0113bb2276b0e7c4ba23
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-08-17 10:46:31 -0300

    att

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
- 
+
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.anbima.com.br/feriados/fer_nacionais/2020.asp');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.anbima.com.br/feriados/fer_nacionais/2020.asp');
 
-  const dateList = await page.evaluate(() => {
-      const nodeList = document.querySelectorAll('.tabela');
+    const dateList = await page.evaluate(() => {
+        const nodeList = document.querySelectorAll('body');
 
-      const dateArray = [...nodeList];
+        const dateArray = [...nodeList];
 
-      const list = dateArray.map(({innerText}) => ({
-          dia: innerText
-      }));
+        const list = dateArray.map(({ innerText }) => ({
+            txt: innerText
+        }));
 
-      let arr = [];
-      list.forEach(teste)
-      function teste(item, index){
-          if(item.dia.indexOf('20') != -1){
-              arr.push(item)
-          }
-      }
+        let arr = [];
+        list.forEach(teste)
+        function teste(item, index) {
+            if (item.txt.indexOf('20') != -1) {
+                let split = item.txt.split(/\t|\n/);
+                split.forEach(function(item) {
+                    if(item.indexOf('20') != -1){
+                        arr.push(item);
+                    }
+                });
+            }
+        }
+        
+        return arr;
+    });
 
-      return arr;
-  });
+    fs.writeFile('date.json', JSON.stringify(dateList, null, 2), err => {
+        if (err) throw new Error('algo deu errado')
+    })
 
-  fs.writeFile('date.json', JSON.stringify(dateList, null, 2), err => {
-      if(err) throw new Error('algo deu errado')
-  })
- 
-  await browser.close();
+    await browser.close();
 })();
